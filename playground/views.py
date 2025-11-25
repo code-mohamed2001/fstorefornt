9c608e8860f91f39a698edd958cade58f39dec28
Mohamed Montaser <info.mmontaser@gmail.com>
2025-11-25 20:19:00 +0200

    finish ORM

--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
 from django.db.models.aggregates import Sum, Min, Max, Avg, Count
 from django.http import HttpResponse
+from django.db import transaction
 from django.db.models import Q, F
-from store.models import Product, Customer, Collection, Order, OrderItem
+from store.models import Product, Customer, Collection, Order, OrderItem, CartItem, Cart
 # Create your views here.
 
 
+@transaction.atomic()
 def say_hello(request):
 
-    result = Product.objects.filter(collection__id=3) \
-        .aggregate(max=Max('unit_price'),
-                   min=Min('unit_price'), avg=Avg('unit_price'))
-    return render(request, 'hello.html', {'name': 'Mohamed Montaser', 'result': result})
+    return render(request, 'hello.html', {'name': 'Mohamed Montaser'})
